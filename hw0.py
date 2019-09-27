3c3db32664b720f4cf92c910927c52e51697870c
SophiaCarpenter <carpes4@uw.edu>
2019-09-27 15:15:18 -0700

    I followed the instructions for the homework.

--- hw0.py
+++ hw0.py
@@ -12,7 +12,7 @@
 # interface, because the vocabulary of possible commands is
 # much greater than can be shown in a graphical interface.
 #
-# Step 1: To run this program, in a terminal window, type:
+# YEP Step 1: To run this program, in a terminal window, type:
 #
 # cd ~/wherever your homework is stored 
 #
@@ -36,30 +36,31 @@ def hello():
 
 hello()
 print("... Let's say that again... \n")
-hello()
 
-# Step 2: Now try deleting the second hello(). Save the file.
-# Run the program again to see the results.
 
-# Step 3: Now insert "hello()" back into the editor buffer
+# YEP Step 2: Now try deleting the second hello(). Save the file.
+# Run the program again to see the results- Erased the repeat
+
+# GOTCHA Step 3: Now insert "hello()" back into the editor buffer
 # below this line. Try using the auto-complete feature. After
 # you type "hel", possible completions should appear.
 # Use the arrow keys or the mouse to select, and hit enter.
+hello()
 
-# Save the file. Run it again to see the results.
+# Save the file. Run it again to see the results.- The second hello paragraph came back!
 
-# Step 4: Now, try a Python program that uses variables. Uncomment
+# YEP Step 4: Now, try a Python program that uses variables. Uncomment
 # the lines below, by removing the # at the start of each line. Fill
 # in the values for length, width, height, and your name.
 # Then save and run the program again.
 
-# length = 0
-# width = 0
-# height = 0
-#
-# me = "<your name here>"
-# print("Volume =", width * length * height)
-# print("My name is", me)
+length = 3
+width = 4
+height = 5
+
+me = "Sophia"
+print("Volume =", width * length * height)
+print("My name is", me)
 
 # Step 5: You can also run Python programs in PyCharm as well.
 # In PyCharm, click File and then New Project. Make sure the
